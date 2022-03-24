997ed8baed9ec978fa9f77d9451f12259583967a
zizudana <zizudana@naver.com>
2022-03-25 02:43:35 +0900

    [20220324] 2048 (Easy)

--- 삼성SW역량테스트/12100.py
+++ 삼성SW역량테스트/12100.py
@@ -10,78 +10,74 @@ def move_up(board):
     head = 0
     tail = 1
     while (tail < n):
-      if (board[i][tail] != 0):
-        if (board[i][head] == 0):
-          board[i][head] = board[i][tail]
-          board[i][tail] = 0
-        elif (board[i][head] == board[i][tail]):
-          board[i][head] = board[i][head] + board[i][tail]
-          board[i][tail] = 0
-        else:
-          head = head + 1
-          if (head != tail):
-            board[i][head] = board[i][tail]
-            board[i][tail] = 0
-      tail = tail + 1
+      if (board[i][tail] != 0): # tail이 0이 아닐때
+        tmp = board[i][tail] # tmp에 tail값을 저장해두고
+        board[i][tail] = 0 # tail은 0으로 초기화
+        if (board[i][head] == 0): 
+          board[i][head] = tmp # head가 0이면 head에 tail값을 저장
+        elif (board[i][head] == tmp): # head와 tail값이 같으면
+          board[i][head] = board[i][head] + tmp # head에 tail값 더해서 저장 (head * 2)
+          head = head + 1 # head++ -> 두번 합쳐지는 것 방지
+        else: # head와 tail 값이 다르면
+          head = head + 1 # head를 하나 옮기고
+          board[i][head] = tmp # head에 tail값 넣기
+      tail = tail + 1 # tail++
   return (board)
 
-def move_down(board):
+def move_down(board): # head, tail을 뒤에서 시작
   for i in range(n):
     head = n - 1
     tail = n - 2
     while (tail > -1):
       if (board[i][tail] != 0):
+        tmp = board[i][tail]
+        board[i][tail] = 0
         if (board[i][head] == 0):
-          board[i][head] = board[i][tail]
-          board[i][tail] = 0
-        elif (board[i][head] == board[i][tail]):
-          board[i][head] = board[i][head] + board[i][tail]
-          board[i][tail] = 0
+          board[i][head] = tmp
+        elif (board[i][head] == tmp):
+          board[i][head] = board[i][head] + tmp
+          head = head - 1
         else:
           head = head - 1
-          if (head != tail):
-            board[i][head] = board[i][tail]
-            board[i][tail] = 0
+          board[i][head] = tmp
       tail = tail - 1
   return (board)
 
-def move_left(board):
+def move_left(board): # move_up과 거의 동일
   for i in range(n):
     head = 0
     tail = 1
     while (tail < n):
       if (board[tail][i] != 0):
+        tmp = board[tail][i]
+        board[tail][i] = 0
         if (board[head][i] == 0):
-          board[head][i] = board[tail][i]
-          board[tail][i] = 0
-        elif (board[head][i] == board[tail][i]):
-          board[head][i] = board[head][i] + board[tail][i]
-          board[tail][i] = 0
+          board[head][i] = tmp
+        elif (board[head][i] == tmp):
+          board[head][i] = board[head][i] + tmp
+          head = head + 1
         else:
           head = head + 1
-          if (head != tail):
-            board[head][i] = board[tail][i]
-            board[tail][i] = 0
+          board[head][i] = tmp
       tail = tail + 1
   return (board)
 
-def move_right(board):
+def move_right(board): # move_down과 거의 동일
   for i in range(n):
     head = n - 1
     tail = n - 2
     while (tail > -1):
       if (board[tail][i] != 0):
+        tmp = board[tail][i]
+        board[tail][i] = 0
         if (board[head][i] == 0):
-          board[head][i] = board[tail][i]
-          board[tail][i] = 0
-        elif (board[head][i] == board[tail][i]):
-          board[head][i] = board[head][i] + board[tail][i]
-          board[tail][i] = 0
+          board[head][i] = tmp
+        elif (board[head][i] == tmp):
+          board[head][i] = board[head][i] + tmp
+          head = head - 1
         else:
           head = head - 1
-          if (head != tail):
-            board[head][i] = board[tail][i]
-            board[tail][i] = 0
+          board[head][i] = tmp
       tail = tail - 1
   return (board)
 
