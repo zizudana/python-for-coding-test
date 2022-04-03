7be2f2444df697366d7912b8d647f2c078af736d
zizudana <zizudana@naver.com>
2022-04-03 17:39:31 +0900

    주석추가

--- 삼성SW역량테스트/14503.py
+++ 삼성SW역량테스트/14503.py
@@ -6,31 +6,30 @@ input = sys.stdin.readline
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-n, m = map(int, input().split())
-r, c, d = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
+n, m = map(int, input().split()) # n * m 직사각형
+r, c, d = map(int, input().split()) # 좌표(r, c) , 방향 d
+board = [list(map(int, input().split())) for _ in range(n)] # 빈칸 0, 벽 1
 
 def clean(r, c, d):
-  global cnt
+  global cnt # 청소한 칸의 개수
   if board[r][c] == 0:  # 현재좌표가 청소할 수 있는지 확인
     board[r][c] = 2
     cnt += 1
-  for i in range(4):
-    nd = (d+3) % 4
+  for _ in range(4):
+    nd = (d+3) % 4 # 왼쪽 방향으로 회전
     nx = r + dx[nd]
     ny = c + dy[nd]
-    if (nx > 0 and ny > 0 and nx < n and ny < m):
-      if board[nx][ny] == 0:  # 왼쪽으로 회전해서 청소할 수 있으면
-       clean(nx, ny, nd)  #다음 좌표와 방향 입력
-       return
+    if board[nx][ny] == 0:  # 왼쪽으로 이동해서 청소할 수 있으면
+      clean(nx, ny, nd)  #다음 좌표와 방향 입력
+      return
     d = nd
   # 네 방향 모두 청소할 수 없는 경우
-  nd = (d+2) % 4
+  nd = (d+2) % 4 # 방향을 뒤로
   nx = r + dx[nd]
-  ny = c + dx[nd]
+  ny = c + dy[nd]
   if board[nx][ny] == 1: # 뒤가 벽인 경우
     return
-  clean(nx, ny, d) # 뒤로 이동할 수 있으면 다음좌표입력, 방향 유지
+  clean(nx, ny, d) # 뒤로 이동할 수 있으면 방향 유지한채로 이동
 
 cnt = 0
 clean(r, c, d)
--- 삼성SW역량테스트/14888.py
+++ 삼성SW역량테스트/14888.py
@@ -1,7 +1,7 @@
 # 연산자 끼워넣기
-n = int(input())
-a_list = list(map(int, input().split()))
-op = list(map(int, input().split()))
+n = int(input()) # n개의 수
+a_list = list(map(int, input().split())) # 순열 a_list
+op = list(map(int, input().split())) # 연산자(+, -, *, /) 개수
 
 def dfs(depth, num):
   global min_num
@@ -12,17 +12,17 @@ def dfs(depth, num):
     return
   else:
     for i in range(4):
-      if op[i] > 0:
-        op[i] -= 1
-        if i == 0:
+      if op[i] > 0: # 연산자가 있으면
+        op[i] -= 1 # 하나 사용
+        if i == 0: # 연산자가 +일때
           dfs(depth+1, num+a_list[depth])
-        elif i == 1:
+        elif i == 1: # 연산자가 -일때
            dfs(depth+1, num-a_list[depth])
-        elif i == 2:
+        elif i == 2: # 연산자가 *일때
           dfs(depth+1, num*a_list[depth])
-        elif i == 3:
+        elif i == 3: # 연산자가 /일때
           dfs(depth+1, int(num/a_list[depth]))
-        op[i] += 1
+        op[i] += 1 # 개수 다시 돌려놓기
 
 min_num = 1e9
 max_num = -1e9
